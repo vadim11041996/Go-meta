f9a0d86c00b3d690ab9f640040605516145dfbd7
vadim11041996 <vadim11041996@hotmail.com>
2018-12-01 13:56:02 +0200

    added worker

--- concurency.go
+++ concurency.go
@@ -1,35 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func Concurency() {
-	c1 := make(chan string)
-	c2 := make(chan string)
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
 
-	go func() {
-		for {
-			c1 <- "Every 500ms"
-			time.Sleep(time.Millisecond * 500)
-		}
-	}()
+	go worker(jobs, results)
 
-	go func() {
-		for {
-			c2 <- "Every 2 secconds"
-			time.Sleep(time.Millisecond * 2000)
-		}
-	}()
-
-	for {
-		select {
-		case msg1 := <-c1:
-			fmt.Println(msg1)
-		case msg2 := <-c2:
-			fmt.Println(msg2)
-		}
+	for i := 0; i < 100; i++ {
+		jobs <- i
 	}
 
+	close(jobs)
+
+	for j := 0; j < 100; j++ {
+		fmt.Println(<-results)
+	}
+}
+
+func worker(jobs <-chan int, results chan<- int) {
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
 }
